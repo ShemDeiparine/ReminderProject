3f6a91e70f4c171ae5685863fb5acd8678b3a538
Rodney Olid <olidrodney@gmail.com>
2022-10-31 10:36:14 -0400

    Add basic menu prompts

---
+++ src/Menu.java
@@ -0,0 +1,11 @@
+public class Menu {
+    public static void menuPrompt() {
+        System.out.println("Welcome to the Reminder App");
+        System.out.println("What would you like to do?");
+        System.out.println("0 to quit");
+        System.out.println("1 to view Reminders");
+        System.out.println("2 to add new Reminders");
+        System.out.println("3 to delete a Reminder");
+        System.out.println("4 to edit a Reminder");
+    }
+}
