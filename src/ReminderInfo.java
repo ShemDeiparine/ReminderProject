b947e47779743776a472fc1799058dc1eb856f78
Rodney Olid <olidrodney@gmail.com>
2022-11-05 19:47:35 -0400

    added reminderInfo

--- src/ReminderInfo.java
+++ src/ReminderInfo.java
@@ -1,7 +1,10 @@
 public class ReminderInfo {
-    String name = "";
-    int month = 0;
-    int day = 0;
-    boolean priority = false;
+    public static String reminderInfo = "";
 
+    public static void setInfo(String info){
+        reminderInfo = info;
+    }
+    private static String getInfo(){
+        return reminderInfo;
+    }
 }
