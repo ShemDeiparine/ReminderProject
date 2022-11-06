ce9c023c1b8a4a313bf48d08e67b91abd0e10762
ShemDeiparine <113954272+ShemDeiparine@users.noreply.github.com>
2022-11-05 20:03:05 -0400

    Add files via upload

---
+++ src/ReminderCompletion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ReminderCompletion {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Reminder Completion: True or False");
+
+    boolean ReminderCompletion = input.nextBoolean();
+
+}
